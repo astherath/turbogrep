929b8202e8dc600acf6c23b40621e5b6ca3c9711
astherath <farceriv@gmail.com>
2021-09-16 11:37:50 -0400

    finally fixed const generic trait code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{App, Arg, ArgMatches};
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct UserInput {
+    regex_string: String,
+    old_term: String,
+    new_term: String,
+}
+
+trait ClapArg<'a, const I: usize> {
+    const A: [&'a str; I];
+    fn get_args<'b>() -> Vec<Arg<'a, 'b>>;
+    fn from_matches(matches: &ArgMatches) -> Self;
+}
+
+impl<'a> ClapArg<'a, 2> for UserInput {
+    const A: [&'a str; 2] = ["ab", "bc"];
+
+    fn get_args<'b>() -> Vec<Arg<'a, 'b>> {
+        vec![Arg::with_name("exp")]
+    }
+
+    fn from_matches(matches: &ArgMatches) -> Self {
+        const REGEX_ARG_NAME: &str = "expr";
+        const OLD_TERM_ARG_NAME: &str = "old";
+        const NEW_TERM_ARG_NAME: &str = "new";
+        Self {
+            regex_string: matches.value_of(REGEX_ARG_NAME).unwrap().to_string(),
+            old_term: matches.value_of(OLD_TERM_ARG_NAME).unwrap().to_string(),
+            new_term: matches.value_of(NEW_TERM_ARG_NAME).unwrap().to_string(),
+        }
+    }
 }
